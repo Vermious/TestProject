27cd95cd226b11422a40791347e64ad09756f3a3
Vermious <michielhendrix@hotmail.com>
2023-05-23 15:18:20 +0200

    [ADD] added storage link

---
+++ public/build/assets/HeaderImage-a822d632.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as t,e as o}from"./app-1b4e6314.js";const c={},s={class:"absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center",src:"/assets/images/groupPadel.jpeg",alt:""};function r(a,n){return t(),o("img",s)}const m=e(c,[["render",r]]);export{m as H};
