ce91a7a32a4c02926cde99fa5c6d472e5323ae1e
Vermious <michielhendrix@hotmail.com>
2023-06-09 13:47:08 +0200

    [ADD] review section

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -75,13 +75,45 @@
         </section>
 
         <!-- Review section -->
+        <section class="w-full px-4">
+            <div class="mx-auto max-w-3xl text-center">
+                <h3 class="mb-6 text-3xl font-bold">Review</h3>
+                <p class="pb-2 text-neutral-500 dark:text-neutral-300 ">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+                error amet numquam iure provident voluptate esse quasi, veritatis
+                totam voluptas nostrum quisquam eum porro a pariatur veniam.
+                </p>
+            </div>
+
+            <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6 mb-8">
+                <figure class="max-w-screen-md mx-auto">
+                    <svg class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+                    </svg> 
+                    <blockquote>
+                        <p class="text-2xl font-medium text-gray-900 dark:text-white">
+                            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+                            error amet numquam iure provident voluptate esse quasi, veritatis
+                            totam voluptas nostrum quisquam eum porro a pariatur veniam."
+                        </p>
+                    </blockquote>
+                    <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                        <img class="w-6 h-6 rounded-full" src="/assets/images/stock-img.jpg" alt="profile picture">
+                        <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                            <div class="pr-3 font-medium text-gray-900 dark:text-white">Name Here</div>
+                            <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">Function here</div>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+        </section>
 
         <!-- Blog -->
         <section class="bg-white dark:bg-gray-900">
             <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
                     <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Onze Blogs</h2>
-                    <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Blijft op de hoogte via onze blogs. Daar vertellen we je graag meer over wat we doen.</p>
+                    <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Blijft op de hoogte via onze blogs. Daar vertellen we je graag meer over de sport padel.</p>
                 </div> 
                 <div class="grid gap-8 lg:grid-cols-2">
                     <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
