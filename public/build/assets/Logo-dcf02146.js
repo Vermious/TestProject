aa07b731d74dfd54936ab5ef6cb0f9463fb8ef7b
Vermious <michielhendrix@hotmail.com>
2023-06-13 13:10:07 +0200

    [ADD] final touches content website

---
+++ public/build/assets/Logo-dcf02146.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,e as s,a as c}from"./app-5581da04.js";const t={},n=c("img",{class:"h-40 w-38",src:"/assets/images/logo.png"},null,-1),r=[n];function a(_,i){return o(),s("div",null,r)}const p=e(t,[["render",a]]);export{p as A};
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -28,13 +28,13 @@
                     en neemt het pijnpunt lege padelbanen en het niet kunnen vinden van padelbuddies weg. Het vullen van banen in 
                     combinatie met het vinden van je padelmaatjes doen wij via een all-in-one app. Padelspelers vinden op deze manier 
                     medespelers om samen te padellen. Daarnaast worden de banen gevuld door het organiseren van evenementen. Deze evenementen 
-                    zijn voor zowel maatschappelijke organisaties als particulieren. Word jij ook gek van de lege padelbanen, download dan 
-                    nu onze app of bezoek onze website! 
+                    zijn voor zowel maatschappelijke organisaties als particulieren. Word jij ook gek van de lege padelbanen, gebruik dan 
+                    nu onze onze website! 
                 </p>
             </div>
         </div>
 
-        <!-- Industry-leading security section -->
+        <!-- Uitleg over wat we doen -->
         <section class="w-full my-36">
             <div class="relative max-w-screen-xl px-8 mx-auto grid grid-cols-12 gap-x-6">
                 <div data-aos="fade-right" class="col-span-12 lg:col-span-6">
@@ -74,14 +74,23 @@
             </div>
         </section>
 
+        <!-- Video -->
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                    <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Kom gezellig padellen</h2>
+                    <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Deze 4 vrienden hebben door middel van onze service elkaar gevonden. Wil jij dit ook?<a class=" hover:text-gray-900" :href="route('register')"> Meld je dan nu aan!</a></p>
+                </div> 
+                <iframe class="w-full aspect-video" width="469" height="833" src="https://www.youtube.com/embed/0WIvtw56onM" title="PadelBuddies, medespelers vinden voor een potje padel!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </section>
+
         <!-- Review section -->
-        <section class="w-full px-4">
+        <section class="w-full pt-8 px-4">
             <div class="mx-auto max-w-3xl text-center">
-                <h3 class="mb-6 text-3xl font-bold">Review</h3>
+                <h3 class="mb-6 text-3xl font-bold">Reviews</h3>
                 <p class="pb-2 text-neutral-500 dark:text-neutral-300 ">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-                error amet numquam iure provident voluptate esse quasi, veritatis
-                totam voluptas nostrum quisquam eum porro a pariatur veniam.
+                    Deze mensen maken al gebruik van onze service. Ben jij de volgende?
                 </p>
             </div>
 
@@ -92,16 +101,16 @@
                     </svg> 
                     <blockquote>
                         <p class="text-2xl font-medium text-gray-900 dark:text-white">
-                            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-                            error amet numquam iure provident voluptate esse quasi, veritatis
-                            totam voluptas nostrum quisquam eum porro a pariatur veniam."
+                            "Super overzichtelijke website, alles was makkelijk te vinden onder de duidelijke kopjes! Op 
+                            deze manier wil ik wel vaker een activiteit boeken."
                         </p>
                     </blockquote>
                     <figcaption class="flex items-center justify-center mt-6 space-x-3">
-                        <img class="w-6 h-6 rounded-full" src="/assets/images/stock-img.jpg" alt="profile picture">
+                        <!-- HERE -->
+                        <img class="rounded-full object-cover h-6 w-6" src="/assets/images/Danny.jpg" alt="profile picture">
                         <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                            <div class="pr-3 font-medium text-gray-900 dark:text-white">Name Here</div>
-                            <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">Function here</div>
+                            <div class="pr-3 font-medium text-gray-900 dark:text-white">Danny Wout</div>
+                            <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">Student</div>
                         </div>
                     </figcaption>
                 </figure>
@@ -124,15 +133,15 @@
                             </span>
                             <span class="text-sm">14 days ago</span>
                         </div>
-                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a :href="route('blog.show')">Title blog 1</a></h2>
-                        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a :href="route('blog.show')">PADELBUDDIES, de ultieme app om medespelers te vinden!</a></h2>
+                        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">In de wereld van racketsporten heeft padel de laatste jaren enorme populariteit gewonnen. Het is een dynamische sport die elementen van tennis en squash vervangt, en het is een geweldige manier om actief te blijven en nieuwe mensen te ontmoeten.</p>
                         <div class="flex justify-between items-center">
                             <div class="flex items-center space-x-4">
                                 <div class="h-8 w-8">
-                                    <img src="/assets/team/Michiel.jpeg" alt="" class="rounded-full object-cover h-full w-full shadow-md" />
+                                    <img src="/assets/images/Mark.jpg" alt="" class="rounded-full object-cover h-full w-full shadow-md" />
                                 </div>
                                 <span class="font-medium dark:text-white">
-                                    Michiel Hendrix
+                                    Mark Scheurs
                                 </span>
                             </div>
                             <a :href="route('blog.show')" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
@@ -142,33 +151,13 @@
                         </div>
                     </article> 
                     <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                        <div class="flex justify-between items-center mb-5 text-gray-500">
-                            <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                                <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                                Article
-                            </span>
-                            <span class="text-sm">14 days ago</span>
-                        </div>
-                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Title blog 2</a></h2>
-                        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Coming Soon!</a></h2>
                         <div class="flex justify-between items-center">
-                            <div class="flex items-center space-x-4">
-                                <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
-                                <span class="font-medium dark:text-white">
-                                    Bonnie Green
-                                </span>
-                            </div>
-                            <a href="#" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                                Read more
-                                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </a>
                         </div>
                     </article>                  
                 </div>  
             </div>
         </section>
-
-        <!-- Video's -->
 
         <!-- Potential partners section -->
 
