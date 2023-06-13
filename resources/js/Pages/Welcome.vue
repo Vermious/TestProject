50c5e2659ca46026a0e0eb42c46dc14b8ff2a3aa
Vermious <michielhendrix@hotmail.com>
2023-06-13 13:25:20 +0200

    [ADD] small adjuztment in the text

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -131,7 +131,7 @@
                                 <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
                                 Article
                             </span>
-                            <span class="text-sm">14 days ago</span>
+                            <!-- <span class="text-sm">14 days ago</span> -->
                         </div>
                         <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a :href="route('blog.show')">PADELBUDDIES, de ultieme app om medespelers te vinden!</a></h2>
                         <p class="mb-5 font-light text-gray-500 dark:text-gray-400">In de wereld van racketsporten heeft padel de laatste jaren enorme populariteit gewonnen. Het is een dynamische sport die elementen van tennis en squash vervangt, en het is een geweldige manier om actief te blijven en nieuwe mensen te ontmoeten.</p>
@@ -145,7 +145,7 @@
                                 </span>
                             </div>
                             <a :href="route('blog.show')" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                                Read more
+                                Lees meer
                                 <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </a>
                         </div>
